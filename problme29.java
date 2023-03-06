18c0fc3618cab2939b1550756f42f26fdc5c0b4a
BeTheWings <be_the_wings@kakao.com>
2023-03-07 01:16:15 +0900

    모든 아나그램 찾기
    1. first
    2. second
    1. 먼저 second를 집어 넣은 map을 만든다(비교용)
    2.만들어 놓은은 map의 길이 -1 만큼 비교용 map을 만든다.
    
    2point 알고리즘을 사용하여 처음에 만든 map과 두번째로 만든 map을 비교한다.
    
    rt는 처음만든 map의 길이에서부터 시작하고 반복문은 first문자열의 길이 만큼 돈다.
    rt에 값이 있을 경우 그 값을 없을 경우 +1을 해준다.
    처음 만든 map과 다음에 만든 map이 같은 경우 count를 ++해준다.
    그 후 lt의 값을 찾아서 그 key해당하는 값이 있는 경우 -1을 해서 0으로 만들어준다.
    lt에 해당하는 값이 0인 경우 해당 key는 map에서 제거해준다. map에서 제거 혹은 조건문이 바르지 않는 경우 lt의 값을 증가시켜준다.

---
+++ problme29.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class problme29 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String first = sc.next();
+		String second = sc.next();
+		
+		ArrayList<Character>sArr = new ArrayList<Character>();
+		/*HashMap에 T는 char value 는 Integer*/
+
+		for(char c :first.toCharArray()) {
+			sArr.add(c);
+		}
+		HashMap<Character, Integer> amap = new HashMap<Character, Integer>();
+		
+		for(char c: second.toCharArray()) {
+			amap.put(c, amap.getOrDefault(c, 0)+1);
+		}
+		
+		
+		int L =second.length()-1;	
+		HashMap<Character, Integer> bmap = new HashMap<Character, Integer>();
+		for(int i=0;i<L;i++) {
+			bmap.put(sArr.get(i), bmap.getOrDefault(sArr.get(i), 0)+1);
+		}
+		
+		int lt =0;
+		int count = 0;
+		for(int rt=L; rt<sArr.size();rt++) {
+			bmap.put(sArr.get(rt), bmap.getOrDefault(sArr.get(rt), 0)+1);
+			if(amap.equals(bmap)) {
+				count++;
+			}
+			bmap.put(sArr.get(lt), bmap.get(sArr.get(lt))-1);
+			
+			if(bmap.get(sArr.get(lt))==0) {
+				bmap.remove(sArr.get(lt));
+			}
+			lt++;
+			
+		}
+					
+		System.out.println(count);
+		
+		
+			
+	}
+
+}
